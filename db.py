23dbad8f2d9fd0c52140ed34201818cd9c020cb5
FarooquiRaiyan <aammaannxyz@gmail.com>
2026-05-19 19:14:58 +0530

    added env

--- db.py
+++ db.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
+import os 
+from dotenv import load_dotenv
 
-DB_PASSWORD = quote_plus("asdf@9876")
+load_dotenv() 
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 DB_URL = f"mysql+pymysql://root:{DB_PASSWORD}@localhost:3306/updated_db_fastapi"
 
